bb8b8e188603a1b599749e94efde802d7b05cf41
iMMoCodes <74028194+iMMoCodes@users.noreply.github.com>
2021-03-16 10:32:30 +0200

    removed useless useEffect and useState

--- src/Components/Question/Question.js
+++ src/Components/Question/Question.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import Container from '@material-ui/core/Container'
 import Button from '@material-ui/core/Button'
 import ButtonGroup from '@material-ui/core/ButtonGroup'
